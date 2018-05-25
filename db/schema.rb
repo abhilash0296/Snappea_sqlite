6ae305357ef7ec9a763d43a671096cfdf879c31c
Abhilash <mulajkarabhi@gmail.com>
2018-05-25 14:12:26 +0530

    Add all files

--- db/schema.rb
+++ db/schema.rb
@@ -14,34 +14,33 @@
 ActiveRecord::Schema.define(version: 20170829113817) do
 
   create_table "restaurantmenuitems", force: :cascade do |t|
-    t.string   "name",          limit: 255
-    t.string   "description",   limit: 255
-    t.string   "category",      limit: 255
-    t.string   "tags",          limit: 255
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-    t.integer  "restaurant_id", limit: 4
+    t.string   "name"
+    t.string   "description"
+    t.string   "category"
+    t.string   "tags"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.integer  "restaurant_id"
   end
 
-  add_index "restaurantmenuitems", ["restaurant_id"], name: "index_restaurantmenuitems_on_restaurant_id", using: :btree
+  add_index "restaurantmenuitems", ["restaurant_id"], name: "index_restaurantmenuitems_on_restaurant_id"
 
   create_table "restaurants", force: :cascade do |t|
-    t.string   "name",        limit: 255
-    t.string   "description", limit: 255
-    t.float    "rating",      limit: 24
-    t.string   "address",     limit: 255
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.string   "name"
+    t.string   "description"
+    t.float    "rating"
+    t.string   "address"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",            limit: 255
-    t.string   "email",           limit: 255
-    t.string   "password_digest", limit: 255
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.string   "api_key",         limit: 255
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "api_key"
   end
 
-  add_foreign_key "restaurantmenuitems", "restaurants"
 end
